ad93e4a901b70180aee74d89d5c3c0cfc1d85371
AlvaroCespedes <aecespedes@miuandes.cl>
2020-03-23 19:56:23 -0300

    first commit

---
+++ Laboratorio 1 AlvaroCespedes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laboratorio_1_AlvaroCespedes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
