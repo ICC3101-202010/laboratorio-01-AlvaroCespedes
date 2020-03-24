f327c2a1d8bba76e90d1ac1bffda5eeb1abacae7
AlvaroCespedes <aecespedes@miuandes.cl>
2020-03-23 22:13:49 -0300

    Better Change

--- Laboratorio 1 AlvaroCespedes/Program.cs
+++ Laboratorio 1 AlvaroCespedes/Program.cs
@@ -1,38 +1,24 @@
 ﻿using System;
 
 namespace Laboratorio_1_AlvaroCespedes
-{
-    public class Persona
+{   
+    public class Mainclass
     {
-        //VARIABLES
-        int maquina = 0;
-        int Humano = 0;
-        public string Name;
-        public string Secondname;
-        Random aleatorio = new Random();
-        public void Lanzar()
+        public class Persona
         {
-            Console.WriteLine("CA CHI PUN!");
-            for(int x = 0; x<3;x++)//Creo elif ciclo, (i++ = i+=1)
+            public string Name;
+            public string Secondname;
+            Random rnd = new Random();
+            public int Throw()
             {
-                Console.WriteLine("Ingrese 0 para piedra");
-                Console.WriteLine("Ingrese 1 para papel");
-                Console.WriteLine("Ingrese 2 para tijera");
-                if (maquina== 0 && Humano == 2)
+                Console.WriteLine("CA CHI PUN!");
+                for (int x = 0; x < 3; x++)//Creo elif ciclo, (i++ = i+=1)
                 {
-                    Console.WriteLine("Gana Maquina");
+                    Console.WriteLine($"{rnd.Next(),15:NO}");
                 }
-                else
-                    {
-    
-                    }
-                if (maquina == 0 && Humano == 1)
-                {
-                    Console.WriteLine("Gana Humano");
-                }
-                if (maquina == 1 && Humano = 2)
-            }
 
+            }
         }
     }
+    
 }
