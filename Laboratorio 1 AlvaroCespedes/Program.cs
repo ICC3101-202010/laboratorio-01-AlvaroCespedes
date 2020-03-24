63c7c864f2af0bc8b7ee80297ef6d7073f7593a3
AlvaroCespedes <aecespedes@miuandes.cl>
2020-03-23 21:33:48 -0300

    first work lab

--- Laboratorio 1 AlvaroCespedes/Program.cs
+++ Laboratorio 1 AlvaroCespedes/Program.cs
@@ -2,12 +2,37 @@
 
 namespace Laboratorio_1_AlvaroCespedes
 {
-    class Program
+    public class Persona
     {
-        static void Main(string[] args)
+        //VARIABLES
+        int maquina = 0;
+        int Humano = 0;
+        public string Name;
+        public string Secondname;
+        Random aleatorio = new Random();
+        public void Lanzar()
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("segundo ");
+            Console.WriteLine("CA CHI PUN!");
+            for(int x = 0; x<3;x++)//Creo elif ciclo, (i++ = i+=1)
+            {
+                Console.WriteLine("Ingrese 0 para piedra");
+                Console.WriteLine("Ingrese 1 para papel");
+                Console.WriteLine("Ingrese 2 para tijera");
+                if (maquina== 0 && Humano == 2)
+                {
+                    Console.WriteLine("Gana Maquina");
+                }
+                else
+                    {
+    
+                    }
+                if (maquina == 0 && Humano == 1)
+                {
+                    Console.WriteLine("Gana Humano");
+                }
+                if (maquina == 1 && Humano = 2)
+            }
+
         }
     }
 }
